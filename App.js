3507151b1ffa1ae979da301c04ce758d44087934
RuslanFrontend <ruslan@b2bdash.io>
2023-12-12 00:15:56 +0200

    Added linear-gradient for app component

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, SafeAreaView, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 import { useFonts } from 'expo-font';
 
 export default function App() {
@@ -14,9 +16,31 @@ export default function App() {
 	}
 
 	return (
-		<StatusBar style="auto" />
+		<LinearGradient
+			colors={ ['#100E24', '#1A162A', '#231D30', '#2C2436', '#3A2F3E', '#83645C'] }
+			locations={ [0, 0.0892, 0.1744, 0.2796, 0.3633, 1] }
+			style={ styles.linearGradient }
+		>
+			<StatusBar style="auto" />
+			<SafeAreaView style={ styles.container }>
+				<View style={ styles.header }></View>
+			</SafeAreaView>
+		</LinearGradient>
 	);
 }
 
 const styles = StyleSheet.create({
+	linearGradient: {
+		flex: 1,
+		paddingLeft: 15,
+		paddingRight: 15,
+		paddingTop: 10,
+		paddingBottom: 45
+	},
+	container: {
+		flex: 1,
+	},
+	header: {
+		height: 90
+	}
 });
